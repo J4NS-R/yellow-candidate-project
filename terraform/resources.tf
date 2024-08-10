ace8788123b6eb0da039a611eab98a4a09e1c9b1
Jans Rautenbach <git@moat.rauten.co.za>
2024-08-10 15:08:17 +0200

    double quotes

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -6,14 +6,14 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_db_instance" "pg" {
-  instance_class      = "db.t3.micro"
-  engine = "postgres"
+  instance_class = "db.t3.micro"
+  engine         = "postgres"
   # aws rds describe-db-engine-versions --engine postgres --output json | jq ".DBEngineVersions[] | .EngineVersion"
   engine_version      = "16.4"
-  allocated_storage = 10
+  allocated_storage   = 10
   db_name             = "yellow"
-  username          = local.terrasecrets['db-username']
-  password          = local.terrasecrets['db-password']
+  username            = local.terrasecrets["db-username"]
+  password            = local.terrasecrets["db-password"]
   apply_immediately   = true
   skip_final_snapshot = true
 }
