0f5896d135f53d0f28a2662b0ac88e653399209e
Jans Rautenbach <14095706+J4NS-R@users.noreply.github.com>
2024-08-10 21:44:47 +0200

    v1.0.1: ignore ssl issues with RDS (#4)

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -2,10 +2,18 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import { env } from '$env/dynamic/private';
 import * as schema from './schema';
+import { Logger } from '$lib/logging';
 
+const log = new Logger('$lib/server/db');
+log.info(`Creating database in NODE_ENV: ${env.NODE_ENV}`);
+
+if (!env.VITE_PG_HOST || !env.VITE_PG_PASS) {
+	log.error('Missing required VITE_PG_* env vars! DB migration will fail.');
+}
 
 const client = postgres(
-	`postgres://${env.VITE_PG_USER}:${env.VITE_PG_PASS}@${env.VITE_PG_HOST}:5432/${env.VITE_PG_DB}`
+	`postgres://${env.VITE_PG_USER}:${env.VITE_PG_PASS}@${env.VITE_PG_HOST}:5432/${env.VITE_PG_DB}`,
+	{ ssl: env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined }
 );
 
 export const db = drizzle(client, { schema });
