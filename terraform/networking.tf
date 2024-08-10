04c6fff18feb93cc993b709d40313c1f5eefb341
Jans Rautenbach <git@moat.rauten.co.za>
2024-08-10 15:25:01 +0200

    networking vibes

---
+++ terraform/networking.tf
@@ -0,0 +1,25 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/24"
+  tags = {
+    Name = "jans-candidate-proj"
+  }
+}
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
+}
+resource "aws_db_subnet_group" "db" {
+  subnet_ids = [aws_subnet.main.id]
+}
+
+# Security groups
+resource "aws_security_group" "db" {
+  name = "jans-candidate-proj-db"
+  ingress {
+    cidr_blocks = [aws_subnet.main.cidr_block]
+    to_port     = aws_db_instance.pg.port
+  }
+  egress {
+    cidr_blocks = [aws_subnet.main.cidr_block]
+  }
+}
--- terraform/resources.tf
+++ terraform/resources.tf
@@ -1,19 +1,17 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/24"
-  tags = {
-    Name = "jans-candidate-proj"
-  }
-}
-
 resource "aws_db_instance" "pg" {
   instance_class = "db.t3.micro"
   engine         = "postgres"
   # aws rds describe-db-engine-versions --engine postgres --output json | jq ".DBEngineVersions[] | .EngineVersion"
-  engine_version      = "16.4"
-  allocated_storage   = 10
-  db_name             = "yellow"
-  username            = local.terrasecrets["db-username"]
-  password            = local.terrasecrets["db-password"]
-  apply_immediately   = true
-  skip_final_snapshot = true
+  engine_version         = "16.4"
+  allocated_storage      = 10
+  db_name                = "yellow"
+  username               = local.terrasecrets["db-username"]
+  password               = local.terrasecrets["db-password"]
+  apply_immediately      = true
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.db.name
+  vpc_security_group_ids = [aws_security_group.db.id]
+  tags = {
+    Name = "jans-candidate-proj"
+  }
 }
